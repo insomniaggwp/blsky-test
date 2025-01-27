f852bef65c4dd09855c5532c92830da1c6df3462
arif hidayat <arifhidayatbiz@gmail.com>
2025-01-27 16:09:01 +0700

    create component ChatBox

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -1,73 +1,24 @@
 import './App.css'
 import { useState } from 'react';
-import { Minimize2, Maximize2, Send } from "react-feather";
+import ChatBox from './components/ChatBox';
 import dummyMessages from './constanta';
 
 function App() {
   const [messages, setMessages] = useState(dummyMessages);
-  const [message, setMessage] = useState();
-  const [isChatBoxDisplay, setIsChatBoxDisplay] = useState(true);
 
-  const onSubmitMessage = (e) => {
-    e.preventDefault();
-
-    if (message.trim()) {
-      const newMessage = { id: crypto.randomUUID(), sender: 'left', message };
-      setMessages((prevMessage) => [...prevMessage, newMessage]);
-      setMessage('');
-    }
+  const onAddMessage = (dataMessage) => {
+    const { sender, message } = dataMessage;
+    const newMessage = { id: crypto.randomUUID(), sender, message };
+    setMessages(prevState => [...prevState, newMessage])
   }
-
-  const RenderToggleHeader = () => {
-    return (
-      <button className="button-close" onClick={() => setIsChatBoxDisplay((prevState) => { return !prevState })}>
-        {isChatBoxDisplay ? <Minimize2 size={24} /> : <Maximize2 size={24} />}
-      </button>
-    );
-  };
 
   return (
     <div className="app">
       <header>
         <h1>Chat App</h1>
         <div className="chat-room-wrapper">
-        <div
-          className="chat-wrapper"
-        >
-          <div className="chat-header">
-            <h3>Left</h3>
-            <div className="chat-icon-wrapper">
-              <RenderToggleHeader />
-            </div>
-          </div>
-          <div className={`chat-content-wrapper`}>
-            <div className="chat-container">
-              {messages.length > 0 &&
-                messages.map((message) => {
-                  return (
-                    <div
-                      key={message.id}
-                      className="chat-bubble"
-                    >
-                      {message.message}
-                    </div>
-                  );
-                })}
-            </div>
-            <div className="chat-content-footer">
-              <form className="input-message-wrapper" onSubmit={onSubmitMessage}>
-                <div className="input-wrapper">
-                  <input value={message} onChange={(e) => setMessage(e.target.value)}></input>
-                </div>
-                <div className="button-wrapper">
-                  <button type="submit">
-                    <Send size={24} />
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
+          <ChatBox sender="left" messages={messages} onAddMessage={onAddMessage}/>
+          <ChatBox sender="right" messages={messages} onAddMessage={onAddMessage}/>
         </div>
       </header>
     </div>
---
+++ chat-app/src/components/ChatBox.jsx
@@ -0,0 +1,103 @@
+import "./ChatBox.css";
+import { useRef, useState, useEffect } from "react";
+import { Minimize2, Maximize2, Send } from "react-feather";
+import PropTypes from "prop-types";
+
+const ChatBox = ({ sender, messages, onAddMessage }) => {
+  const [isChatDisplay, setIsChatDisplay] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const messageEndRef = useRef(null);
+  const bodyChatRef = useRef(null);
+
+  const onToggleChatDisplay = () => {
+    setIsChatDisplay((prevState) => {
+      return !prevState;
+    });
+  };
+
+  const RenderToggleHeader = () => {
+    return (
+      <button className="button-close" onClick={onToggleChatDisplay}>
+        {isChatDisplay ? <Minimize2 size={24} /> : <Maximize2 size={24} />}
+      </button>
+    );
+  };
+
+  const onChangeMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const onSubmitMessage = (e) => {
+    e.preventDefault();
+
+    if (message.trim()) {
+      onAddMessage({ message, sender });
+      setMessage("");
+      bodyChatRef.current.scrollTop = bodyChatRef.current.scrollHeight;
+    }
+  };
+
+  const scrollToBottom = () => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(scrollToBottom, [messages]);
+
+  return (
+    <div
+      className={`chat-wrapper ${sender} ${isChatDisplay ? "show" : "hidden"}`}
+    >
+      <div className="chat-header">
+        <h3>{sender}</h3>
+        <div className="chat-icon-wrapper">
+          <RenderToggleHeader />
+        </div>
+      </div>
+      <div className={`chat-content-wrapper`}>
+        <div className="chat-container" ref={bodyChatRef}>
+          {messages.length > 0 &&
+            messages.map((chat) => {
+              return (
+                <div
+                  key={chat.id}
+                  className={`chat-bubble ${
+                    sender === chat.sender ? "sender" : "receiver"
+                  }`}
+                >
+                  {chat.message}
+                </div>
+              );
+            })}
+          <div ref={messageEndRef} />
+        </div>
+        <div className="chat-content-footer">
+          <form className="input-message-wrapper" onSubmit={onSubmitMessage}>
+            <div className="input-wrapper">
+              <input value={message} onChange={onChangeMessage}></input>
+            </div>
+            <div className="button-wrapper">
+              <button type="submit">
+                <Send size={24} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ChatBox.propTypes = {
+  sender: PropTypes.string,
+  onAddMessage: PropTypes.func.isRequired,
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      sender: PropTypes.string.isRequired,
+      message: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default ChatBox;
